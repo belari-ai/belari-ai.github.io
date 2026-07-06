deefa2dc4922ed91e77a6caa4d8a65c05ac95f9f
npyati <npyati@gmail.com>
2026-07-06 13:07:00 -0500

    Deploy 2026-07-06T18:07:00Z

---
+++ assets/notFound-mkVnSkrp.js
@@ -0,0 +1,1 @@
+import{j as e,S as r,m as s}from"./nav-DRTlAJt3.js";import{H as n,L as o}from"./Text-gsv1fDEI.js";import{P as i}from"./Page-D-fOOXMD.js";function t(){return e.jsx(i,{children:e.jsxs(r,{children:[e.jsx(n,{level:1,children:"Not found."}),e.jsxs(o,{children:["That page isn’t here. ",e.jsx("a",{href:"/",children:"Back to belari →"})]})]})})}s(e.jsx(t,{}));
