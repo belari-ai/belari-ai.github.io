55b7fbff9e220843e14a3dcd88a1853ce24672a1
npyati <npyati@gmail.com>
2026-07-09 14:27:39 -0500

    Deploy 2026-07-09T19:27:39Z

---
+++ assets/notFound-D83px5sY.js
@@ -0,0 +1,1 @@
+import{j as e,S as r,m as s}from"./nav-CcjlGHe0.js";import{H as n,L as o}from"./Text-gFplEk6i.js";import{P as i}from"./Page-B2FIHYr_.js";function t(){return e.jsx(i,{children:e.jsxs(r,{children:[e.jsx(n,{level:1,children:"Not found."}),e.jsxs(o,{children:["That page isn’t here. ",e.jsx("a",{href:"/",children:"Back to belari →"})]})]})})}s(e.jsx(t,{}));
